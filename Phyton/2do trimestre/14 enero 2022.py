3003b0054c05773325da91641799d733bd6fc81e
ame <104370757+amesantos@users.noreply.github.com>
2022-06-03 14:14:50 -0500

    Add files via upload

---
+++ Phyton/2do trimestre/14 enero 2022.py
@@ -0,0 +1,10 @@
+#volver al codigo interactivo
+x=int(input())
+if x<10:
+  print('smaller')
+  if x<=20 and x>=10:
+    print('in the midlle')
+    if x>20:
+      print('bigger')
+
+print('finis')
