1eaf8bb9ec708d5057af8a28e704330f657b513d
ame <104370757+amesantos@users.noreply.github.com>
2022-06-03 14:08:39 -0500

    Add files via upload

---
+++ Phyton/1er Trimestre/22 noviembre 2021.py
@@ -0,0 +1,17 @@
+numero1=int(input('ingresa un numero:'))
+numero2=int(input('ingresa un numero:'))
+print(numero1+numero2)
+
+numero1=int(input('ingresa un numero:'))
+numero2=int(input('ingresa un numero:'))
+print(numero1-numero2)
+
+numero1=int(input('ingresa un numero:'))
+numero2=int(input('ingresa un numero:'))
+print(numero1*numero2)
+
+numero1=int(input('ingresa un numero:'))
+numero2=int(input('ingresa un numero:'))
+print(numero1/numero2)
+
+
