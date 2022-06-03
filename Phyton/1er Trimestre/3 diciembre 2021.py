e7ceafb1136dbdc0aa9c89d406edf1543ce12e96
ame <104370757+amesantos@users.noreply.github.com>
2022-06-03 13:58:04 -0500

    Add files via upload

---
+++ Phyton/1er Trimestre/3 diciembre 2021.py
@@ -0,0 +1,46 @@
+print("Welcome to CINEPOLIS ")
+print("insert your name")
+
+a=2
+
+while(a==2):
+  print('hola mundo')
+
+print('reprobados')
+    
+print("Welcome", nombre)
+
+print("what food do you want to buy?")
+
+food1=input('item 1: ')
+price1=int(input('price: '))
+
+food2=input('item 2: ')
+price2=int(input('price: '))
+
+food3=input('item 3: ')
+price3=int(input('price: '))
+
+food4=input('item 4: ')
+price4=int(input('price: '))
+
+food5=input('item 5: ')
+price5=int(input('price: '))
+
+item1= price1
+item2= price2
+item3= price3
+item4= price4
+item5= price5
+total= item1+item2+item3+item4+item5
+purchase=total*0.16
+print(total)
+
+print('-º-º-º-ºTicket-º-º-º-º')
+print("1.",item1,"price: ",price1)
+print("2.",item2,"price: ",price2)
+print("3.",item3,"price: ",price3)
+print("4.",item4,"price: ",price4)
+print("5.",item5,"price: ",price5)
+print(total)
+print("Thanks for buying, enjoy your movie")
