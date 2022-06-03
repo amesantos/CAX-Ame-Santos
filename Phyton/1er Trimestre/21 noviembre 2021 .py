295dea4cc7d53686be096f38ca00db7f92a1981a
ame <104370757+amesantos@users.noreply.github.com>
2022-06-03 14:06:20 -0500

    Add files via upload

---
+++ Phyton/1er Trimestre/21 noviembre 2021 .py
@@ -0,0 +1,63 @@
+print('cual es tu nombre: ')
+nombre=input()
+print('saludos ', nombre)
+
+print('te gusta escuchar musica?: ')
+musica=input()
+print('wow que bien ', )
+
+print('artista favorito: ')
+artista=input()
+print('es muy bueno ',)
+
+print('cual es tu comida favorita: ')
+comida=input()
+print('me gusta esa comida',)
+
+print('frio o calor: ')
+frio=input()
+print('yo prefiero el frio',)
+
+print('pelicula favorita?: ')
+pelicula=input()
+print('esa pelicula me encanta',)
+
+print('te gusta la nutella?: ')
+nutella=input()
+print('a mi me encanta',)
+
+print('actor favorito?: ')
+actor=input()
+print('el mio es tom holland y dylan minnette',)
+
+print('te gustan las fiestas?: ')
+fiesta=input()
+print('oh que bien',)
+
+print('frio o calor: ')
+frio=input()
+print('yo prefiero el frio',)
+
+print('dia o noche: ')
+dia=input()
+print('yo prefiero el dia y a veces las noches',)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
