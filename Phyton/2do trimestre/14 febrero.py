cc989ed165e70d5654213f7f9b7532b0ca3964f3
ame <104370757+amesantos@users.noreply.github.com>
2022-06-28 21:04:27 -0500

    Add files via upload

---
+++ Phyton/2do trimestre/14 febrero.py
@@ -0,0 +1,10 @@
+var1= 30
+
+if var1==10:
+  print ("The if statement   is true!")
+elif var1 ==20:
+  print("The first elif is true!")
+elif var1 == 30:
+  print ("The second elif is true!")
+else:
+  print ("All apare false!")
