64ad9eafc7c13db4840fc8b8edd36d3c5d67bf6b
ame <104370757+amesantos@users.noreply.github.com>
2022-06-28 21:12:11 -0500

    Add files via upload

---
+++ Phyton/3rd trimestre/11 marzo.py
@@ -0,0 +1,18 @@
+while True:
+    x=input("ingresa la palabra:")
+    print(x)
+
+    if(x=="reprobados"):
+        break
+print("fin del juego")
+input()
+
+suma=7
+for x in range (100):
+    suma=suma+2
+    input()
+  
+    if (suma==23):
+        break
+    #continue 
+    print(suma)
