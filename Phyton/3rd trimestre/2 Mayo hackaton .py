cdd17568928e36b28981f3907ca9f5566c7bca63
ame <104370757+amesantos@users.noreply.github.com>
2022-06-28 21:13:14 -0500

    Add files via upload

---
+++ Phyton/3rd trimestre/2 Mayo hackaton .py
@@ -0,0 +1,29 @@
+while True:
+  print("Bienvenido a chatbot Whatsapp")
+  print("ingrese 1 para reporte de fallas, ingrese 2 para pagos, ingrese 3 para promociones ")
+  menu=input("¿Cuál es su problema?:")
+  
+  if (menu=="1"):
+    print("reporte de fallas")
+    print("television o internet")
+    print("Ingrese 1 para television")
+    print("Ingrese 2 para internet")
+    repor=input("¿Cual es su falla?")
+    if (repor=="1"):
+      print("Su número de reporte es 20223467")
+      print("gracias por su reporte,lo tomaremos en cuenta más adelante") 
+    if(repor=="2"):
+      print("Su número de reporte es 20223467")
+      print("gracias por su reporte,lo tomaremos en cuenta más adelante") 
+      
+  if (menu=="2"):
+    print("Forma de pagos")
+    input("Ingrese número de tarjeta de pago:")
+    print("El pago fue recibido") 
+     
+  if (menu=="3"):
+     print("Promociones")
+     print("recibes el 20% de descuento en pagos en efectivo")
+     print("tienes una promocion en tu tarjeta club de cinepolis, llevate dos combos familia, 2 x 40  ")
+     print("has visitado vidanta unas tres veces al año! tienes una promocion en una habitacion vip a tan solo 10 mil pesos")
+    
